f447dbe4446fe978af6c4ffa260231e5428ff39c
liliansnunes <liliansnunes@outlook.com.br>
2021-09-14 19:53:02 +0000

    up

---
+++ src/main/java/application/models/GenerationType.java
@@ -0,0 +1,7 @@
+package application.models;
+
+public class GenerationType {
+
+    public static final javax.persistence.GenerationType IDENTITY = null;
+
+}
---
+++ src/main/java/application/models/Livro.java
@@ -0,0 +1,29 @@
+package application.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="livros")
+
+public class Livro{
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private int id;
+public int getId() {
+    return id;
+}
+public void setId(int id) {
+    this.id = id;
+}
+private String titulo;
+public String getTitulo() {
+    return titulo;
+}
+public void setTitulo(String titulo) {
+    this.titulo = titulo;
+}   
+}
